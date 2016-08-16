35e972eda724eea8968a4161c54775fd78f542fa
kwongsuphasawat <kwongsuphasawat@twitter.com>
2016-08-16 13:57:22 -0700

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,16 @@
 module.exports = {
   "extends": "airbnb",
   "plugins": [
-    "react"
-  ]
+    "react",
+    "mocha"
+  ],
+  "rules": {
+    "mocha/no-exclusive-tests": "error"
+  },
+  globals: {
+    describe: true,
+    it: true,
+    expect: true,
+    before: true
+  }
 };
