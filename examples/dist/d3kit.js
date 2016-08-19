24619beae13812a21faffea560204c31df4c3beb
Robert Harris <github.com@trebor.org>
2016-08-19 14:15:57 -0700

    build distrobution for rc3

--- examples/dist/d3kit.js
+++ examples/dist/d3kit.js
@@ -653,7 +653,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  return selection.on('click', function (d, i) {
 	    dispatch.call(prefix + 'Click', this, d, i);
 	  }).on('mouseover', function (d, i) {
-	    dispatch.call(prefix + 'MouseOvern', this, d, i);
+	    dispatch.call(prefix + 'MouseOver', this, d, i);
 	  }).on('mousemove', function (d, i) {
 	    dispatch.call(prefix + 'MouseMove', this, d, i);
 	  }).on('mouseout', function (d, i) {
