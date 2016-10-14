7b99b28a5959bff737187261d493577138a33d38
kwongsuphasawat <kwongsuphasawat@twitter.com>
2016-10-14 00:31:24 -0700

    remove unused files

--- tasks/image.js
+++
@@ -1,7 +0,0 @@
-const $ = require('gulp-load-plugins')();
-import {newer, wrap} from './core.js';
-
-export const compileImage = wrap(function(src, dest){
-  return newer(src, dest)
-    .pipe($.imagemin())
-});
